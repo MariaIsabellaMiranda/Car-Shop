d3c620f151e6497f2639261d0b17de6ac63f121b
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-10-26 13:43:36 -0300

    feat: Implementado MongoModel, de forma genérica para lidar com várias coleções

---
+++ src/models/MongoModel.ts
@@ -0,0 +1,32 @@
+import { Model } from 'mongoose';
+import { IModel } from '../interfaces/IModel';
+
+abstract class MongoModel<T> implements IModel<T> {
+  protected _model: Model<T>;
+
+  constructor(model: Model<T>) {
+    this._model = model;
+  }
+
+  public async create(obj: T): Promise<T> {
+    return this._model.create(obj);
+  }
+
+  public async read(paramsRead: string): Promise<T[]> {
+    return this._model.find({ paramsRead });
+  }
+
+  public async readOne(_id: string): Promise<T | null> {
+    return this._model.findOne({ _id });
+  }
+
+  // public update(paramsUpdate: string, obj: T): Promise<T | null> {
+  //   return this._model.findOneAndUpdate(paramsUpdate, obj);
+  // }
+
+  // public delete(paramsDelete: string): Promise<T | null> {
+  //   return this._model.findOneAndDelete({ paramsDelete });
+  // }
+}
+
+export default MongoModel;
