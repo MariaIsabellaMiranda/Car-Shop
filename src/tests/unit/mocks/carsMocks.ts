4b4a6b1b10b1a83358c6bdf722ac56a824e50ec8
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-10-26 19:34:49 -0300

    feat: Criado arquivo com alguns mocks que serão utilizados nos testes

---
+++ src/tests/unit/mocks/carsMocks.ts
@@ -0,0 +1,49 @@
+import { ICar } from '../../../interfaces/ICar';
+
+export const carsMockStatusNot:ICar = {
+  model: "Ferrari Maranello",
+  year: 1963,
+  color: "red",
+  buyValue: 3500000,
+  doorsQty: 2,
+  seatsQty: 2,
+}
+
+export const carsMockWithIdStatusNot:ICar & { _id: string }= {
+  _id: "4edd40c86762e0fb12000003",
+  model: "Ferrari Maranello",
+  year: 1963,
+  color: "red",
+  buyValue: 3500000,
+  doorsQty: 2,
+  seatsQty: 2,
+}
+
+export const carsMockStatusTrue:ICar = {
+  status: true,
+  model: "Ferrari Maranello",
+  year: 1963,
+  color: "red",
+  buyValue: 3500000,
+  doorsQty: 2,
+  seatsQty: 2,
+}
+
+export const carsMockStatusFalse:ICar = {
+  status: false,
+  model: "Ferrari Maranello",
+  year: 1963,
+  color: "red",
+  buyValue: 3500000,
+  doorsQty: 2,
+  seatsQty: 2,
+}
+
+export const errorCarsMock:ICar = {
+  model: "Ferrari Maranello",
+  year: 1963,
+  color: "red",
+  buyValue: 3500000,
+  doorsQty: 0,
+  seatsQty: 2,
+}
