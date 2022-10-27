10f7dd73cd06ed81f5405e4bd7dee35ca5b47b85
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-10-26 23:28:57 -0300

    feat: Implementados métodos read e readOne na camada Model da coleção Cars

--- src/models/MongoModel.ts
+++ src/models/MongoModel.ts
@@ -12,13 +12,13 @@ abstract class MongoModel<T> implements IModel<T> {
     return this._model.create(obj);
   }
 
-  // public async read(paramsRead: string): Promise<T[]> {
-  //   return this._model.find({ paramsRead });
-  // }
+  public async read(): Promise<T[]> {
+    return this._model.find();
+  }
 
-  // public async readOne(_id: string): Promise<T | null> {
-  //   return this._model.findOne({ _id });
-  // }
+  public async readOne(_id: string): Promise<T | null> {
+    return this._model.findOne({ _id });
+  }
 
   // public update(paramsUpdate: string, obj: T): Promise<T | null> {
   //   return this._model.findOneAndUpdate(paramsUpdate, obj);
