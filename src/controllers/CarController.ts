b7a8634ce83d80a37e9079400a0015f33042aa91
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-10-26 23:28:19 -0300

    feat: Implementados métodos read e readOne na camada controller da coleção Cars

--- src/controllers/CarController.ts
+++ src/controllers/CarController.ts
@@ -5,10 +5,24 @@ import { ICar } from '../interfaces/ICar';
 class CarController {
   constructor(private _service: IService<ICar>) { }
 
-  public async create(req: Request, res: Response) {
+  public async create(req: Request, res: Response<ICar>) {
     const data = await this._service.create(req.body);
 
     return res.status(201).json(data);
+  }
+
+  public async read(_req: Request, res: Response<ICar[]>) {
+    const data = await this._service.read();
+
+    return res.status(200).json(data);
+  }
+
+  public async readOne(req: Request, res: Response<ICar | null>) {
+    const { id } = req.params;
+
+    const data = await this._service.readOne(id);
+
+    return res.status(200).json(data);
   }
 }
 
