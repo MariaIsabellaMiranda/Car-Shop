fe81c2c5553d9763324dedbe56883e1ce48e1ca9
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-10-26 23:31:04 -0300

    feat: Implementado métodos read e readOnde na interface IService

--- src/interfaces/IService.ts
+++ src/interfaces/IService.ts
@@ -1,5 +1,5 @@
 export interface IService<T> {
   create(obj: T):Promise<T>,
-  // read(paramsRead: string):Promise<T[]>,
-  // readOne(_id: string):Promise<T | null>,
+  read():Promise<T[]>,
+  readOne(_id: string):Promise<T | null>,
 }
