9128a3ab945cb4ac9fb7aa88571f62c211cc6bd7
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-10-26 23:29:35 -0300

    feat: Criadas rotas para métodos read e readOne da coleção Cars

--- src/routes/Car.routes.ts
+++ src/routes/Car.routes.ts
@@ -9,6 +9,8 @@ const carModel = new CarModel();
 const carService = new CarService(carModel);
 const carController = new CarController(carService);
 
-route.post('/cars', (req, res) => carController.create(req, res));
+route.post('/', (req, res) => carController.create(req, res));
+route.get('/', (req, res) => carController.read(req, res));
+route.get('/:id', (req, res) => carController.readOne(req, res));
 
 export default route;
