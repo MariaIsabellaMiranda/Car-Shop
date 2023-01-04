610c36e67b073cf1e943cc111227a0245f8cc63f
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2023-01-04 18:06:02 -0300

    feat: [CarService] Implementado método update.

--- src/services/CarService.ts
+++ src/services/CarService.ts
@@ -24,10 +24,21 @@ class CarService implements IService<ICar> {
 
   public async readOne(_id: string): Promise<ICar | null> {
     if (_id.length < 24) throw new Error(ErrorTypes.InvalidMongoId);
-
+    
     const car = await this._cars.readOne(_id);
-  
-    if (!car) throw new Error(ErrorTypes.EntityNotFound);
+
+    if (!car) throw Error(ErrorTypes.EntityNotFound);
+
+    return car;
+  }
+
+  public async update(_id: string, obj: ICar): Promise<ICar | null> {
+    if (_id.length < 24) throw new Error(ErrorTypes.InvalidMongoId);
+    if (!obj || JSON.stringify(obj) === '{}') throw Error(ErrorTypes.BodyNotFound);
+
+    const car = await this._cars.update(_id, obj);
+
+    if (!car) throw Error(ErrorTypes.EntityNotFound);
 
     return car;
   }
