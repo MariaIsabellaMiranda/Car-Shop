a006f701067c334721f1f8266f60cfa16364282b
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2023-01-04 18:01:15 -0300

    feat: [CarController.ts] Criado controller para método update.

--- src/controllers/CarController.ts
+++ src/controllers/CarController.ts
@@ -5,22 +5,30 @@ import { ICar } from '../interfaces/ICar';
 class CarController {
   constructor(private _service: IService<ICar>) { }
 
-  public async create(req: Request, res: Response<ICar>) {
+  public async create(req: Request, res: Response<ICar>):Promise<Response> {
     const data = await this._service.create(req.body);
 
     return res.status(201).json(data);
   }
 
-  public async read(_req: Request, res: Response<ICar[]>) {
+  public async read(_req: Request, res: Response<ICar[]>):Promise<Response> {
     const data = await this._service.read();
 
     return res.status(200).json(data);
   }
 
-  public async readOne(req: Request, res: Response<ICar | null>) {
+  public async readOne(req: Request, res: Response<ICar | null>):Promise<Response> {
     const { id } = req.params;
 
     const data = await this._service.readOne(id);
+
+    return res.status(200).json(data);
+  }
+
+  public async update(req: Request, res: Response<ICar | unknown>):Promise<Response> {
+    const { id } = req.params;
+
+    const data = await this._service.update(id, req.body);
 
     return res.status(200).json(data);
   }
