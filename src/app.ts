7398ad3d186314d6f34210086c14c564966d9ff2
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2023-01-04 20:00:30 -0300

    feat: [app.ts] Adicionada nova rota para motorcycles.

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import 'express-async-errors';
 import errorHandler from './middlewares/error';
 import carRouter from './routes/Car.routes';
+import motorcycleRouter from './routes/Motorcycle.routes';
 
 const app = express();
 app.use(express.json());
 
 app.use('/cars', carRouter);
+app.use('/motorcycles', motorcycleRouter);
 
 app.use(errorHandler);
 
