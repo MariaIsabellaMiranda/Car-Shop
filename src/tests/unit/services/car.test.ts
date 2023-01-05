d39309c68eed6a31bec26ae4601a29b85b1edc7f
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2023-01-05 20:21:04 -0300

    test: [car.test.ts] Implementados testes da camada Service para a coleção Cars.

--- src/tests/unit/services/car.test.ts
+++ src/tests/unit/services/car.test.ts
@@ -6,9 +6,10 @@ import { ZodError } from 'zod';
 import {
   carsMockStatusNot,
   carsMockWithIdStatusNot,
-  carsMockStatusTrue,
-  carsMockStatusFalse,
-  errorCarsMock
+  errorBodyNotFound,
+  errorCarsMock,
+  errorCharacters,
+  errorObjectNotFound,
 } from '../mocks/carsMocks';
 
 
@@ -17,29 +18,171 @@ describe('CarService', () => {
   const carService = new CarService(carModel);
 
   before(async () => {
-    sinon
-      .stub(carModel, 'create')
-      .resolves(carsMockWithIdStatusNot);
+    sinon.stub(carModel, 'create').resolves(carsMockWithIdStatusNot);
+    sinon.stub(carModel, 'read').resolves([carsMockWithIdStatusNot]);
+    sinon.stub(carModel, 'readOne')
+    .onCall(0).resolves(carsMockWithIdStatusNot)
+    .onCall(1).resolves(null);
+    sinon.stub(carModel, 'update')
+    .onCall(0).resolves(carsMockWithIdStatusNot)
+    .onCall(1).resolves(null);
+    sinon.stub(carModel, 'delete')
+    .onCall(0).resolves(carsMockWithIdStatusNot)
+    .onCall(1).resolves(null);
   });
 
   after(()=>{
     sinon.restore();
   })
 
-  it('Cria coleção Cars com sucesso', async () => {
-    const newCar = await carService.create(carsMockStatusNot);
+  describe('Método create', () => {
+    describe('Testa caso de sucesso', () => {
+      it('Cria coleção Cars com sucesso', async () => {
+        const newCar = await carService.create(carsMockStatusNot);
+    
+        expect(newCar).to.be.deep.equal(carsMockWithIdStatusNot);
+      });
+    })
 
-    expect(newCar).to.be.deep.equal(carsMockWithIdStatusNot);
+    describe('Testa caso de erro', () => {
+      it('Erro ao tentar criar coleção Car', async () => {
+        let error;
+      		try {
+      			await carService.create(errorCarsMock);
+      		} catch (err) {
+      			error = err
+      		}
+    
+      		expect(error).to.be.instanceOf(ZodError);
+      });
+    });
   });
 
-  it('Erro ao tentar criar coleção Car', async () => {
-    let error;
-			try {
-				await carService.create(errorCarsMock);
-			} catch (err) {
-				error = err
-			}
+  describe('Método read', () => {
+    describe('Testa caso de sucesso', () => {
+      it('Busca lista com todos os carros', async () => {
+        const cars = await carService.read();
+    
+        expect(cars).to.be.deep.equal([carsMockWithIdStatusNot]);
+      });
+    })
+  });
 
-			expect(error).to.be.instanceOf(ZodError);
+  describe('Método readOne', () => {
+    describe('Testa caso de sucesso', () => {
+      it('Busca um carro específico de acordo com id passado', async () => {
+        const car = await carService.readOne(carsMockWithIdStatusNot._id);
+    
+        expect(car).to.be.deep.equal(carsMockWithIdStatusNot);
+      });
+    })
+
+    describe('Testa caso de erro', () => {
+      it('Erro ao passar um id com menos de 24 caracteres', async () => {
+        let error;
+      		try {
+      			await carService.readOne("4edd40c86762e0fb120002");
+      		} catch (err: any) {
+      			error = err
+      		}
+    
+      		expect(error.message).to.be.equal(errorCharacters);
+      });
+
+      it('Erro ao passar um id inválido', async () => {
+        let error;
+      		try {
+      			await carService.readOne("4edd40c86762e0fb12000226");
+      		} catch (err: any) {
+      			error = err
+      		}
+    
+      		expect(error.message).to.be.equal(errorObjectNotFound);
+      });
+    });
+  });
+
+  describe('Método update', () => {
+    describe('Testa caso de sucesso', () => {
+      it('Atualiza um carro no db de acordo com id passado', async () => {
+        const car = await carService.update(carsMockWithIdStatusNot._id, carsMockStatusNot);
+    
+        expect(car).to.be.deep.equal(carsMockWithIdStatusNot);
+      });
+    })
+
+    describe('Testa caso de erro', () => {
+      it('Erro ao passar um id com menos de 24 caracteres', async () => {
+        let error;
+      		try {
+      			await carService.update("4edd40c86762e0fb120002", carsMockStatusNot);
+      		} catch (err: any) {
+      			error = err
+      		}
+    
+      		expect(error.message).to.be.equal(errorCharacters);
+      });
+
+      it('Erro ao passar um body vazio', async () => {
+        let error;
+      		try {
+      			await carService.update("4edd40c86762e0fb12000203", {} as typeof carsMockStatusNot);
+      		} catch (err: any) {
+      			error = err
+      		}
+    
+      		expect(error.message).to.be.equal(errorBodyNotFound);
+      });
+
+      it('Erro ao passar um id inválido', async () => {
+        let error;
+      		try {
+      			await carService.update("4edd40c86762e0fb12000226", carsMockStatusNot);
+      		} catch (err: any) {
+      			error = err
+      		}
+    
+      		expect(error.message).to.be.equal(errorObjectNotFound);
+      });
+    });
+  });
+
+  describe('Método delete', () => {
+    describe('Testa caso de sucesso', () => {
+      it('Exclui um carro com sucesso de acordo com id passado', async () => {
+        let error;
+      		try {
+      			await carService.delete(carsMockWithIdStatusNot._id);
+      		} catch (err: any) {
+      			error = err
+      		}
+    
+      		expect(error).to.be.undefined;
+      });
+    });
+
+    describe('Testa caso de erro', () => {
+      it('Erro ao passar um id com menos de 24 caracteres', async () => {
+        let error;
+      		try {
+      			await carService.delete("4edd40c86762e0fb120002");
+      		} catch (err: any) {
+      			error = err
+      		}
+    
+      		expect(error.message).to.be.equal(errorCharacters);
+      });
+
+      it('Erro ao passar um id inválido', async () => {
+        let error;
+      		try {
+      			await carService.delete("4edd40c86762e0fb12000226");
+      		} catch (err: any) {
+      			error = err
+      		}
+    
+      		expect(error.message).to.be.equal(errorObjectNotFound);
+      });
+    });
   });
 });
