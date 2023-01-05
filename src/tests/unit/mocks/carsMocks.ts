5ed9bd2edabd5be035b8a25d387b6c15f2cdbaa6
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2023-01-05 20:22:16 -0300

    feat: [carsMocks.ts] Adicionados e excluídos alguns mocks para testes da coleção Cars.

--- src/tests/unit/mocks/carsMocks.ts
+++ src/tests/unit/mocks/carsMocks.ts
@@ -19,35 +19,6 @@ export const carsMockWithIdStatusNot:ICar & { _id: string }= {
   seatsQty: 2,
 }
 
-export const carsMockWithIdStatusNotArray:ICar[] & { _id: string }[]= [{
-  _id: "4edd40c86762e0fb12000003",
-  model: "Ferrari Maranello",
-  year: 1963,
-  color: "red",
-  buyValue: 3500000,
-  doorsQty: 2,
-  seatsQty: 2,
-}]
-
-export const carsMockStatusTrue:ICar = {
-  status: true,
-  model: "Ferrari Maranello",
-  year: 1963,
-  color: "red",
-  buyValue: 3500000,
-  doorsQty: 2,
-  seatsQty: 2,
-}
-
-export const carsMockStatusFalse:ICar = {
-  status: false,
-  model: "Ferrari Maranello",
-  year: 1963,
-  color: "red",
-  buyValue: 3500000,
-  doorsQty: 2,
-  seatsQty: 2,
-}
 
 export const errorCarsMock:ICar = {
   model: "Ferrari Maranello",
@@ -55,5 +26,19 @@ export const errorCarsMock:ICar = {
   color: "red",
   buyValue: 3500000,
   doorsQty: 0,
+  seatsQty: 1,
+}
+
+export const invalidIdCarsMock:ICar & { _id: string }= {
+  _id: "4edd40c86762e0fb12000203",
+  model: "Ferrari Maranello",
+  year: 1963,
+  color: "red",
+  buyValue: 3500000,
+  doorsQty: 2,
   seatsQty: 2,
 }
+
+export const errorCharacters: String = 'InvalidMongoId';
+export const errorBodyNotFound: String = 'BodyNotFound';
+export const errorObjectNotFound: String = 'EntityNotFound';
